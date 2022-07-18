b9baa9c25d5f77ad852015b3aa5fa4977693336c
Jayesh Gohel <jayesh.gohel@gmail.com>
2022-07-18 20:05:49 +0530

    Start gallery component

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,17 @@
 import "pannellum";
-
-let threesixtyViewers = [];
-
-let threesixtyObserver = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      threesixtyViewers[entry.target.id] = pannellum.viewer(entry.target.id, {
-        panorama: `/images/${entry.target.dataset.story}/360/${entry.target.id}.jpg`,
-        mouseZoom: false,
-        keyboardZoom: false,
-        autoLoad: true,
-        showControls: false,
-        preview: '/images/paper.jpg',
-      })
-    } else {
-      if (threesixtyViewers[entry.target.id]) {
-        threesixtyViewers[entry.target.id].destroy()
-      }
-    }
-  })
-})
+import "pannellum/build/pannellum.css";
 
 let els = document.querySelectorAll('.threesixty')
 
 if (els.length) {
   els.forEach((el) => {
-    threesixtyObserver.observe(el)
+    pannellum.viewer(el.id, {
+      panorama: `/images/${el.dataset.story}/360/${el.id}.jpg`,
+      mouseZoom: false,
+      keyboardZoom: false,
+      autoLoad: true,
+      showControls: false,
+      preview: '/images/paper.jpg',
+    })
   })
 }
