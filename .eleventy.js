58dd7405317b77b5c4bb3926aa25a531ca405df7
Jayesh Gohel <jayesh.gohel@gmail.com>
2023-02-11 19:11:16 +0530

    Minor fixes as per PPT

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addFilter('sortByOrder', (values) => {
+    let vals = [...values];
+    return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
+  });
 
   return {
     dir: {
